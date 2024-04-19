c818ba14b5626b42b9f87e0d261b27d1e7f4a618
revensif <revensif@mail.ru>
2024-04-19 21:23:03 +0300

    fixed checkstyle

--- scrapper/src/main/java/edu/java/configuration/ClientConfigurationProperties.java
+++ scrapper/src/main/java/edu/java/configuration/ClientConfigurationProperties.java
@@ -3,9 +3,7 @@ package edu.java.configuration;
 import edu.java.utils.RetryPolicy;
 import jakarta.validation.constraints.NotNull;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.Bean;
 import org.springframework.validation.annotation.Validated;
-import reactor.util.retry.Retry;
 
 @Validated
 @ConfigurationProperties(prefix = "client", ignoreUnknownFields = false)
