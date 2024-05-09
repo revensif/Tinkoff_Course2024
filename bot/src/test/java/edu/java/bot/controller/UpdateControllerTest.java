8cfeb8de7a70fd5a7395cca88f6046cade39441e
revensif <revensif@mail.ru>
2024-05-09 17:33:17 +0300

    trying to fix build

--- bot/src/test/java/edu/java/bot/controller/UpdateControllerTest.java
+++ bot/src/test/java/edu/java/bot/controller/UpdateControllerTest.java
@@ -12,6 +12,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.servlet.MockMvc;
 import org.testcontainers.shaded.com.fasterxml.jackson.databind.ObjectMapper;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
@@ -19,6 +20,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(UpdateController.class)
+@DirtiesContext
 @AutoConfigureMockMvc
 public class UpdateControllerTest {
 
